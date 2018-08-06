e3e95433f4e470bba19df3eed23f8fe288dedd6e
João Araújo <juauzaraujo@gmail.com>
2018-08-06 21:59:59 +0100

    text change

--- src/components/header.js
+++ src/components/header.js
@@ -77,7 +77,7 @@ const Header = ({ siteTitle }, props) => (
       <h1 style={styles.titleStyles.titleH1}>João Araújo</h1>
       <AllText>
       <Text top={"16px"}bot={"24px"} right={"54px"}>
-        <p style={styles.textStyles.textNormal}><span style={styles.textStyles.textBold}>I am a Digital Product Designer</span> from Portugal, currently living in Porto. In the last 2 years had the opportunity to work as a designer for both startups and big companies.</p>
+        <p style={styles.textStyles.textNormal}><span style={styles.textStyles.textBold}>I am a Digital Product Designer</span> from Portugal, currently living in Porto. In the last 2 years I've work as a designer for both startups and big companies from many parts of the world.</p>
       </Text>
       <Text>
         <p style={styles.textStyles.textNormal}>If you want to know more about my work or discuss how I can bring value to your project, feel free to <a href="#">contact me through my e-mail hi@juauz.com</a></p>
